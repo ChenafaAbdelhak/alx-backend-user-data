3c06ab955ef161869cf486dabc208e424c62924c
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-07 23:58:47 +0100

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,9 @@
 from typing import List
 import re
 import logging
+import mysql.connector
+import os
+from mysql.connector import Error
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -30,6 +33,31 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Function that returns a connector to the database"""
+    
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
+    db_port = os.getenv('PERSONAL_DATA_DB_PORT', 3306)  # Default MySQL port
+
+    connector = None
+
+    try:
+        connector = mysql.connector.connect(
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        )
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connector
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -46,3 +74,22 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
+
+
+def main():
+    """main function"""
+
+    columns = ["name", "email", "phone", "ssn", "password"]
+
+    query = f"SELECT {', '.join(columns)} FROM users;"
+
+    info_logger = get_logger()
+    connection = get_db()
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        for row in rows:
+            msg = '; '.join(f"{columns[i]}={row[i]}" for i in range(len(columns))) + ";"
+            info_logger.info(msg)
