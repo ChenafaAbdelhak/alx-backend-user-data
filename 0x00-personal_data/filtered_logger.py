3b6258da65efae8a22668263a8834ce87665998b
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-07 19:14:39 +0100

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/urs/bin/env python3
+"""module contain filter_datum function"""
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str):
+    """Function that returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
