189cc7a6af2f92d01cc812b52ef60501cf19b636
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-07 23:29:26 +0100

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ import re
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Function that returns the log message obfuscated"""
@@ -13,6 +16,18 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """function that return logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+    logger.propagate = False
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
@@ -29,5 +44,5 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         record.msg = filter_datum(self.fields, self.REDACTION,
-                                      record.getMessage(), self.SEPARATOR)
+                                  record.getMessage(), self.SEPARATOR)
         return super().format(record)
