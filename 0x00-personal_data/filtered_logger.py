aa62ce5d4228766dea1c6245ea596280eedfeab7
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-07 19:17:01 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """Function that returns the log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
