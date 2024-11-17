eba42cd45bce7428778a411289cbea4d3216149e
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-17 20:53:28 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""module for session authentication"""
-from auth import Auth
+"""module for session authentication
+"""
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """a class to implement the session authentication
     """
+    pass
