d1b48278c362d2b61f62e9fec79cd78a9e164f83
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 16:57:33 +0100

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,7 +42,7 @@ def login() -> str:
     session_id = AUTH.create_session(email)
 
     response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie({"session_id": session_id})
+    response.set_cookie("session_id", session_id)
     return response
 
 
