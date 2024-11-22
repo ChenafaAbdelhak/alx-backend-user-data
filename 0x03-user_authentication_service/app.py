b5e892dd3333a260b26ffce4e0734e03033a6ffc
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 18:48:49 +0100

    reset_password oute

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,20 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token() -> str:
+    """reset password route"""
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+
+    try:    
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    else:
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
