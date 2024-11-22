52916fabb7aaef52905f3c2a4507024bc152aa29
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 18:39:42 +0100

    create reset token method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,7 +65,7 @@ def profile() -> str:
     session_id = request.cookies.get("session_id")
     if not session_id:
         return abort(403)
-    
+
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
