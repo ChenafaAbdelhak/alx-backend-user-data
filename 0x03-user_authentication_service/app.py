da22732c14f3ba09e2e5621e84ca41f323480dc6
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 19:38:18 +0100

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,7 +97,7 @@ def update_password() -> str:
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updates"}), 200
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
 
