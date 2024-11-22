c932e7433fcb20eca6635274116910dc37a9de5d
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 11:16:26 +0100

    test generating uuid

--- 0x03-user_authentication_service/main_4.py
+++ 0x03-user_authentication_service/main_4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """test password hash"""
-from auth import _hash_passwword
+from auth import _hash_password
 
 
 print(_hash_password("Holberton"))
---
+++ 0x03-user_authentication_service/main_9.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""main file"""
+from auth import _generate_uuid
+
+print(_generate_uuid())
