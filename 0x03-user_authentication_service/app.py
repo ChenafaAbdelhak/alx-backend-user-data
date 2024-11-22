10a8daa7eaadd1119cd3fa4e296f2eb05182cac4
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-22 17:44:35 +0100

    destroy session route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -45,6 +45,19 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
     return response
 
+
+@app.route("sessions", methods=["DELETE"])
+def logout() -> str:
+    """method to handle logout"""
+    cookie = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(cookie)
+
+    if not user:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
